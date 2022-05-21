643e28aba92419baac8514cf0015a9147700533f
Lukas Thompson <65417978+LukasThompson@users.noreply.github.com>
2022-05-21 16:05:14 -0400

    Player SFX Script Added
    Added player sfx script, updated player controller script to play sfx from coin on collision

---
+++ Assets/Scripts/PlayerSFX.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSFX : MonoBehaviour
+{
+    public AudioSource playerAudioSource;
+    public AudioClip[] jumpAudio;
+    public AudioClip coinCollected;
+
+    private int randomSFX;
+
+    private void Start()
+    {
+        playerAudioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAudioSource.PlayOneShot(jumpAudio[randomSFX]);
+        }
+    }
+
+    void RandomAudioEffect()
+    {
+        randomSFX = Random.Range(0, jumpAudio.Length);
+    }
+}
