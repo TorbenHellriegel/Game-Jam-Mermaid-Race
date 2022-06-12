5577439a3a73a89dabf842cc00dacb69bb6bd486
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-06-12 12:42:59 +0200

    Added control toggle

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Toggle ControlToggle;
+
     public GameObject character1;
     public GameObject character2;
     public GameObject character3;
@@ -14,6 +17,12 @@ public class MainMenu : MonoBehaviour
     {
         string difficulty = PlayerPrefs.GetString("Difficulty", "Medium");
         PlayerPrefs.SetString("Difficulty", difficulty);
+        PlayerPrefs.SetString("ControlType", PlayerPrefs.GetString("ControlType", "Tap"));
+    }
+
+    void Update()
+    {
+        Debug.Log(PlayerPrefs.GetString("ControlType"));
     }
 
     public void ExitButton()
@@ -29,15 +38,27 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Game Closed");
     }
 
-    public void swipeControlsEnabled(bool enabled)
+    public void SwipeControlsEnabled()
     {
-        if(enabled)
+        if(ControlToggle.isOn)
         {
             PlayerPrefs.SetString("ControlType", "Swipe");
         }
         else
         {
             PlayerPrefs.SetString("ControlType", "Tap");
+        }
+    }
+
+    public void SetControlsToggle()
+    {
+        if(PlayerPrefs.GetString("ControlType", "Tap") == "Swipe" )
+        {
+            ControlToggle.isOn = true;
+        }
+        else if(PlayerPrefs.GetString("ControlType", "Tap") == "Tap" )
+        {
+            ControlToggle.isOn = false;
         }
     }
 
